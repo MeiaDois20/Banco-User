733d3e78eedfcaf9b5903e9b056f965b4cb150c0
Thalys Ravel <ravelthalys66@proton.me>
2025-09-06 14:33:27 -0300

    Update ContaController.java

--- src/util/ContaController.java
+++ src/util/ContaController.java
@@ -3,8 +3,13 @@ import java.util.Scanner;
 import entities.Conta;
 
 public class ContaController {
+    private Scanner sc;
+
+    public ContaController(Scanner sc) {
+        this.sc = sc;
+    }
+
     public void deposito(Conta conta, double valor) {
-        Scanner sc = new Scanner(System.in);
         while (valor < 0.0) {
             System.out.println();
             System.out.println("ERROR[] Valor não identificado");
@@ -15,13 +20,11 @@ public class ContaController {
     }
 
     public void saque(Conta conta, double valor) {
-        Scanner sc = new Scanner(System.in);
         while (valor > conta.getValor() || valor <= 0.0) {
             System.out.println("ERROR[] Digite um valor valído para saque.");
             System.out.print("Digite um valor para saque: ");
             valor = sc.nextDouble();
             System.out.println();
-
         }
         conta.saque(valor);
     }
